4255fd69e672c020e41a98e121ffe7de5e910bba
Jan Stefl <Honza.Stefl@seznam.cz>
2025-04-02 22:43:58 +0200

    validation for model

--- services/order_service/app/models/order.py
+++ services/order_service/app/models/order.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class OrderCreate(BaseModel):
-    user_id: int
-    product_id: int
+    user_id: int = Field(..., gt=0, description="Must be a positive integer")
+    product_id: int = Field(..., gt=0, description="Must be a positive integer")
 
 class Order(OrderCreate):
     id: int
